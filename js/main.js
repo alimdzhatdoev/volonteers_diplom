19ee49df3fa2b2bd246667f900208250cbab6ba4
alimdzhatdoev <alimdzhatdoev1@gmail.com>
2024-04-06 17:31:40 +0300

    Update pages

--- js/main.js
+++ js/main.js
@@ -1,12 +1,4 @@
 $(document).ready(function() {
-    $('.menuItem').click(function() {
-        $('.menuItem').removeClass('activeMenuItem');
-        $(this).addClass('activeMenuItem');
-        
-        let text = $(".menuItem_title", this).text();
-        $(".header_title").html(text)
-    });
-
     $('.news_item__bottom___title').each(function(){
         var text = $(this).text();
         let count = 100;
