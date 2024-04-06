5c6a70b44f5c97c278e6261450386b67f3ca1d00
alimdzhatdoev <alimdzhatdoev1@gmail.com>
2024-04-06 14:03:22 +0300

    Update main slider, banner, news

--- js/main.js
+++ js/main.js
@@ -6,4 +6,14 @@ $(document).ready(function() {
         let text = $(".menuItem_title", this).text();
         $(".header_title").html(text)
     });
+
+    $('.news_item__bottom___title').each(function(){
+        var text = $(this).text();
+        let count = 100;
+
+        if(text.length > count){
+            text = text.substring(0, count) + '...';
+            $(this).text(text);
+        }
+    });
 });
